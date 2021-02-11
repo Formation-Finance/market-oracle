e7741cc3e8f123337ee53743be3083784fd3ad71
Okura Ikeda <ikeda.okura@gmail.com>
2021-02-10 23:13:45 -0300

    truffle init

---
+++ truffle.js
@@ -0,0 +1,38 @@
+const connectionConfig = require('frg-ethereum-runners/config/network_config.json');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+require('dotenv').config();
+
+module.exports = {
+  networks: {
+    ganacheUnitTest: connectionConfig.ganacheUnitTest,
+    gethUnitTest: connectionConfig.gethUnitTest,
+    testrpcCoverage: connectionConfig.testrpcCoverage,
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.ROPSTEN_PROVIDER),
+      network_id: 3,
+      gas: 2000000,
+      gasPrice: 10000000000
+    },
+    mainnet: {
+      ref: 'mainnet-prod',
+      network_id: 1,
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.MAINNET_PROVIDER),
+      gasPrice: 50000000000
+    }
+  },
+  mocha: {
+    enableTimeouts: false
+  },
+  compilers: {
+    solc: {
+      version: '0.4.24',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }
+  }
+};
